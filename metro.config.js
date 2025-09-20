cc0824c419243c58f7845b125c4a4d52a01f10ce
CarFag80 <carmine.faggiano@gmail.com>
2025-09-20 13:24:26 +0200

    Fix caller-callsite using Metro resolver

--- metro.config.js
+++ metro.config.js
@@ -1,14 +1,19 @@
 const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
 
 const config = getDefaultConfig(__dirname);
 
-// 👇 Alias per sostituire caller-callsite con polyfill
+// Configure resolver to use local polyfill for caller-callsite
 config.resolver = {
   ...config.resolver,
+  alias: {
+    ...(config.resolver?.alias ?? {}),
+    'caller-callsite': path.resolve(__dirname, 'polyfills/caller-callsite.js'),
+  },
   extraNodeModules: {
     ...(config.resolver?.extraNodeModules ?? {}),
-    'caller-callsite': require.resolve('./polyfills/caller-callsite.js'),
-  }
+    'caller-callsite': path.resolve(__dirname, 'polyfills/caller-callsite.js'),
+  },
 };
 
 module.exports = config;
